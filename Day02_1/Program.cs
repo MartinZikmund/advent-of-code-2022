3d7d76dc6e9c0761b57def3b6eae36fa27e31d89
Martin Zikmund <martin@zikmund.dev>
2022-12-02 15:15:23 -0500

    Day 2

---
+++ Day02_1/Program.cs
@@ -0,0 +1,34 @@
+﻿long totalScore = 0;
+
+while (await Console.In.ReadLineAsync() is string line)
+{
+    var parts = line.Split(" ");
+    var opponentShape = (Shape)(parts[0][0] - 'A');
+    var ourShape = (Shape)(parts[1][0] - 'X');
+    totalScore += CalculateRoundScore(opponentShape, ourShape);
+}
+
+await Console.Out.WriteLineAsync(totalScore.ToString());
+
+long CalculateRoundScore(Shape opponent, Shape we)
+{
+    var shapeScore = GetShapeScore(we);
+    var roundScore = 3;
+    if (opponent != we)
+    {
+        int ourShapeIndex = (int)we;
+        int opponentShapeIndex = (int)opponent;
+        roundScore = ourShapeIndex == (opponentShapeIndex + 1) % 3 ? 6 : 0;
+    }
+
+    return shapeScore + roundScore;
+}
+
+long GetShapeScore(Shape shape) => (int)shape + 1;
+
+enum Shape
+{
+    Rock,
+    Paper,
+    Scissors
+}
---
+++ Day02_2/Program.cs
@@ -0,0 +1,46 @@
+﻿int totalScore = 0;
+
+while (await Console.In.ReadLineAsync() is string line)
+{
+    var parts = line.Split(" ");
+    var opponentShape = (Shape)(parts[0][0] - 'A');
+    var outcome = (RoundOutcome)(parts[1][0] - 'X');
+    totalScore += CalculateRoundScore(opponentShape, outcome);
+}
+
+await Console.Out.WriteLineAsync(totalScore.ToString());
+
+int CalculateRoundScore(Shape opponent, RoundOutcome outcome)
+{
+    var roundScore = (int)outcome * 3;
+    var shapeScore = 0;
+    if (outcome == RoundOutcome.Draw)
+    {
+        shapeScore = GetShapeScore(opponent);
+    }
+    else if (outcome == RoundOutcome.Win)
+    {
+        shapeScore = GetShapeScore((Shape)(((int)opponent + 1) % 3));
+    }
+    else
+    {
+        shapeScore = GetShapeScore((Shape)(((int)opponent + 2) % 3));
+    }
+    return shapeScore + roundScore;
+}
+
+int GetShapeScore(Shape shape) => (int)shape + 1;
+
+enum Shape
+{
+    Rock,
+    Paper,
+    Scissors
+}
+
+enum RoundOutcome
+{
+    Loss,
+    Draw,
+    Win
+}
