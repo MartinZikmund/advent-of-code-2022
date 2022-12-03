05dc652e9c1dae439b5ab2fb74874be31e109c68
Martin Zikmund <martin@zikmund.dev>
2022-12-03 15:56:31 -0500

    Day 3

---
+++ Day03_1/Program.cs
@@ -0,0 +1,11 @@
+﻿long totalPriority = 0;
+
+while (await Console.In.ReadLineAsync() is string line)
+{
+    totalPriority += line
+        .Take(line.Length / 2)
+        .Intersect(line.TakeLast(line.Length / 2))
+        .Sum(c => c <= 'Z' ? (c - 'A' + 27) : (c - 'a' + 1));
+}
+
+await Console.Out.WriteLineAsync(totalPriority.ToString());
---
+++ Day03_2/Program.cs
@@ -0,0 +1,14 @@
+﻿long totalPriority = 0;
+
+while (
+    await Console.In.ReadLineAsync() is string line &&
+    await Console.In.ReadLineAsync() is string line2 &&
+    await Console.In.ReadLineAsync() is string line3)
+{
+    totalPriority += line
+        .Intersect(line2)
+        .Intersect(line3)
+        .Sum(c => c <= 'Z' ? (c - 'A' + 27) : (c - 'a' + 1));
+}
+
+await Console.Out.WriteLineAsync(totalPriority.ToString());
