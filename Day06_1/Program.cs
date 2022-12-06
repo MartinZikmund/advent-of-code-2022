d4417c791d16ae6b3687227a4882088cdd38c079
Martin Zikmund <martin@zikmund.dev>
2022-12-06 16:51:32 -0500

    Day 6

---
+++ Day06_1/Program.cs
@@ -0,0 +1,26 @@
+﻿var line = File.ReadAllText("input.txt");
+
+var window = new HashSet<char>();
+for (int i = 3; i < line.Length; i++)
+{
+    window.Clear();
+	var isValid = true;
+	for (int previousCharacter = 0; previousCharacter < 4; previousCharacter++)
+	{
+		var character = line[i - previousCharacter];
+        if (window.Contains(character))
+		{
+			isValid = false;
+			break;
+		}
+		else
+		{
+			window.Add(character);
+		}
+	}
+    if (isValid)
+	{
+		Console.WriteLine(i + 1);
+		break;
+	}
+}
---
+++ Day06_2/Program.cs
@@ -0,0 +1,28 @@
+﻿var line = File.ReadAllText("input.txt");
+
+var windowSize = 14;
+
+var window = new HashSet<char>();
+for (int i = windowSize - 1; i < line.Length; i++)
+{
+    window.Clear();
+	var isValid = true;
+	for (int previousCharacter = 0; previousCharacter < windowSize; previousCharacter++)
+	{
+		var character = line[i - previousCharacter];
+        if (window.Contains(character))
+		{
+			isValid = false;
+			break;
+		}
+		else
+		{
+			window.Add(character);
+		}
+	}
+    if (isValid)
+	{
+		Console.WriteLine(i + 1);
+		break;
+	}
+}
