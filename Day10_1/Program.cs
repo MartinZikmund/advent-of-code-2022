f6f32a44f65422044a4e4feb0a83a55bef8b7738
Martin Zikmund <martin@zikmund.dev>
2022-12-10 12:50:01 -0500

    Day 10

---
+++ Day10_1/Program.cs
@@ -0,0 +1,45 @@
+﻿var instructions = File.ReadAllLines("input.txt");
+
+long sumOfSignals = 0;
+long x = 1;
+
+var breakpoints = new int[]
+{
+    20,
+    60,
+    100,
+    140,
+    180,
+    220
+};
+var nextBreakpointId = 0;
+
+var tick = 0;
+foreach (var instruction in instructions)
+{
+    if (breakpoints[nextBreakpointId] == tick ||
+        (instruction != "noop" && breakpoints[nextBreakpointId] <= tick + 2))
+    {
+        var signalStrength = x * breakpoints[nextBreakpointId];
+        sumOfSignals += signalStrength;
+        nextBreakpointId++;
+        if (breakpoints.Length == nextBreakpointId)
+        {
+            break;
+        }
+    }
+    
+    if (instruction == "noop")
+    {
+        tick++;
+    }
+    else
+    {
+        var addSplit = instruction.Split(' ');
+        int value = int.Parse(addSplit[1]);
+        x += value;
+        tick += 2;
+    }
+}
+
+Console.WriteLine(sumOfSignals.ToString());
---
+++ Day10_2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+var instructions = File.ReadAllLines("input.txt");
+
+long sumOfSignals = 0;
+long x = 1;
+
+var breakpoints = new int[]
+{
+    20,
+    60,
+    100,
+    140,
+    180,
+    220
+};
+var nextBreakpointId = 0;
+
+StringBuilder outputBuilder = new();
+
+var tick = 0;
+foreach (var instruction in instructions)
+{
+    ProcessCycle();
+        
+    if (instruction != "noop")
+    {
+        ProcessCycle();
+        var addSplit = instruction.Split(' ');
+        int value = int.Parse(addSplit[1]);
+        x += value;
+    }
+}
+
+Console.WriteLine(outputBuilder.ToString());
+
+void ProcessCycle()
+{
+    var spritePosition = x;
+    if (spritePosition - 1 <= tick && tick <= spritePosition + 1)
+    {
+        outputBuilder.Append("#");
+    }
+    else
+    {
+        outputBuilder.Append(".");
+    }
+
+    tick++;
+    if (tick % 40 == 0)
+    {
+        outputBuilder.AppendLine();
+        tick = 0;
+    }
+}
