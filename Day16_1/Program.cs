ae768d2af370acc7a9f97aec596f1f2931f0fa4d
Martin Zikmund <martin@zikmund.dev>
2022-12-18 21:44:44 +0100

    Day 16

---
+++ Day16_1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("input.txt");
+
+var regex = new Regex("Valve ([A-Z]*) .*=([0-9]*);.*valve.? (.*)");
+
+var valves = new Dictionary<string, Valve>();
+
+int counter = 0;
+foreach (var line in lines)
+{
+    var match = regex.Match(line);
+    var name = match.Groups[1].Value;
+    var value = int.Parse(match.Groups[2].Value);
+    var connections = match.Groups[3].Value.Split(", ");
+
+    var valve = new Valve(counter++, name, value, connections);
+    valves.Add(name, valve);
+}
+
+long bestPressure = 0;
+Dictionary<(int, int, long), long> bestPressures = new Dictionary<(int, int, long), long>();
+
+FindSolution("AA", 0, 30, 0);
+
+void FindSolution(string position, int totalPressure, int timeLeft, long openValves)
+{
+    var currentValve = valves[position];
+    if (bestPressures.TryGetValue((currentValve.Id, timeLeft, openValves), out var best) &&
+        best >= totalPressure)
+    {
+        return;
+    }
+
+    bestPressures[(currentValve.Id, timeLeft, openValves)] = totalPressure;
+
+    timeLeft--;
+    if (timeLeft == 0)
+    {
+        if (totalPressure > bestPressure)
+        {
+            bestPressure = totalPressure;
+        }
+        return;
+    }
+
+    // Open valve
+    if (currentValve.Pressure > 0 && (openValves & (1L << currentValve.Id)) == 0)
+    {
+        openValves |= 1L << currentValve.Id;
+        FindSolution(position, totalPressure + currentValve.Pressure * timeLeft, timeLeft, openValves);
+        openValves &= ~(1 << currentValve.Id);
+    }
+
+    // Move
+    foreach (var tunnel in currentValve.Tunnels)
+    {
+        FindSolution(tunnel, totalPressure, timeLeft, openValves);
+    }
+}
+
+Console.WriteLine(bestPressure);
+
+class Valve
+{
+    public Valve(int id, string name, int pressure, string[] tunnels)
+    {
+        Id = id;
+        Name = name;
+        Pressure = pressure;
+        Tunnels = tunnels;
+    }
+
+    public int Id { get; }
+
+    public string Name { get; }
+
+    public int Pressure { get; }
+
+    public string[] Tunnels { get; }
+}
---
+++ Day16_2/Program.cs
@@ -0,0 +1,141 @@
+﻿using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("input.txt");
+
+var regex = new Regex("Valve ([A-Z]*) .*=([0-9]*);.*valve.? (.*)");
+
+var valves = new Dictionary<string, Valve>();
+
+int counter = 0;
+foreach (var line in lines)
+{
+    var match = regex.Match(line);
+    var name = match.Groups[1].Value;
+    var value = int.Parse(match.Groups[2].Value);
+    var connections = match.Groups[3].Value.Split(", ");
+
+    var valve = new Valve(counter++, name, value, connections);
+    valves.Add(name, valve);
+}
+
+Dictionary<(int, int, int, long), int> bestPressures = new Dictionary<(int, int, int, long), int>();
+Dictionary<(int, int, int, long), int> bestPressuresFromConfiguration = new Dictionary<(int, int, int, long), int>();
+
+long bestSolutionSoFar = 0;
+
+long bestSolution = FindSolution("AA", "AA", 0, 26, 0);
+
+int FindSolution(string ourPosition, string elephantPosition, int totalPressure, int timeLeft, long openValves)
+{
+    var ourValve = valves[ourPosition];
+    var elephantValve = valves[elephantPosition];
+
+    var minId = Math.Min(ourValve.Id, elephantValve.Id);
+    var maxId = Math.Max(ourValve.Id, elephantValve.Id);
+
+    if (bestPressures.TryGetValue((minId, maxId, timeLeft, openValves), out var best) &&
+        best >= totalPressure)
+    {
+        return 0;
+    }
+
+    bestPressures[(minId, maxId, timeLeft, openValves)] = totalPressure;
+
+    timeLeft--;
+
+    if (bestPressuresFromConfiguration.TryGetValue((minId, maxId, timeLeft, openValves), out var bestPressureFromConfiguration))
+    {
+        totalPressure += bestPressureFromConfiguration;
+        timeLeft = 0;
+    }    
+
+    if (timeLeft == 0)
+    {
+        bestSolutionSoFar = Math.Max(totalPressure, bestSolutionSoFar);
+        return totalPressure;
+    }
+
+    var weCanOpen = ourValve.Pressure > 0 && (openValves & (1L << ourValve.Id)) == 0;
+    var elephantCanOpen = elephantValve.Pressure > 0 && (openValves & (1L << elephantValve.Id)) == 0 && ourPosition != elephantPosition;
+
+    var bestForCurrentConfiguration = 0;
+
+    // Both open
+    if (weCanOpen && elephantCanOpen)
+    {
+        openValves |= 1L << ourValve.Id;
+        openValves |= 1L << elephantValve.Id;
+        bestForCurrentConfiguration = Math.Max(
+            bestForCurrentConfiguration, 
+            FindSolution(ourPosition, elephantPosition, totalPressure + ourValve.Pressure * timeLeft + elephantValve.Pressure * timeLeft, timeLeft, openValves));
+        openValves &= ~(1L << elephantValve.Id);
+        openValves &= ~(1L << ourValve.Id);
+    }
+
+    // Only we open
+    if (weCanOpen)
+    {
+        openValves |= 1L << ourValve.Id;
+        foreach (var elephantTunnel in elephantValve.Tunnels)
+        {
+            bestForCurrentConfiguration = Math.Max(
+                bestForCurrentConfiguration,
+                FindSolution(ourPosition, elephantTunnel, totalPressure + ourValve.Pressure * timeLeft, timeLeft, openValves));
+        }
+        openValves &= ~(1L << ourValve.Id);
+    }
+
+    // Only elephant opens
+    if (elephantCanOpen)
+    {
+        openValves |= 1L << elephantValve.Id;
+        foreach (var ourTunnel in ourValve.Tunnels)
+        {
+            bestForCurrentConfiguration = Math.Max(
+                bestForCurrentConfiguration,
+                FindSolution(ourTunnel, elephantPosition, totalPressure + elephantValve.Pressure * timeLeft, timeLeft, openValves));
+        }
+        openValves &= ~(1L << elephantValve.Id);
+    }
+
+    // Move
+    foreach (var ourTunnel in ourValve.Tunnels)
+    {
+        foreach (var elephantTunnel in elephantValve.Tunnels)
+        {
+            bestForCurrentConfiguration = Math.Max(
+                bestForCurrentConfiguration, 
+                FindSolution(ourTunnel, elephantTunnel, totalPressure, timeLeft, openValves));
+        }
+    }
+
+    var bestFromHere = bestForCurrentConfiguration - totalPressure;
+
+    if (!bestPressuresFromConfiguration.TryGetValue((minId, maxId, timeLeft, openValves), out bestPressureFromConfiguration) ||
+        bestFromHere > bestPressureFromConfiguration)
+    {
+        bestPressuresFromConfiguration[(minId, maxId, timeLeft, openValves)] = bestFromHere;
+    }
+    return bestForCurrentConfiguration;
+}
+
+Console.WriteLine(bestSolution);
+
+class Valve
+{
+    public Valve(int id, string name, int pressure, string[] tunnels)
+    {
+        Id = id;
+        Name = name;
+        Pressure = pressure;
+        Tunnels = tunnels;
+    }
+
+    public int Id { get; }
+
+    public string Name { get; }
+
+    public int Pressure { get; }
+
+    public string[] Tunnels { get; }
+}
